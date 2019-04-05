67b72952b469ee95091f248179c858ec64696718
BuildTools <mato.dipcar@gmail.com>
2019-04-05 17:21:03 +0200

    Create Items.java
    Declare all money items

---
+++ src/main/java/sk/dipo/money/items/Items.java
@@ -0,0 +1,22 @@
+package sk.dipo.money.items;
+
+import net.minecraft.item.Item;
+
+public class Items {
+	
+	public static Item cent1;
+	public static Item cent2;
+	public static Item cent5;
+	public static Item cent10;
+	public static Item cent20;
+	public static Item cent50;
+	public static Item euro1;
+	public static Item euro2;
+	public static Item euro5;
+	public static Item euro10;
+	public static Item euro20;
+	public static Item euro50;
+	public static Item euro100;
+	public static Item euro200;
+	public static Item euro500;
+}
